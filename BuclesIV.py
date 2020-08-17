89627b9b072a9e665ed1def3005afba32629af4b
Lucho <quirogaluistomas@gmail.com>
2020-08-16 22:30:03 -0300

    Mi primer commit con git

---
+++ BuclesIV.py
@@ -0,0 +1,10 @@
+#Continue
+
+for letra in "python":
+
+    if letra == "h":
+        continue
+        #El continue ignora el resto del bucle y salta a la siguiente iteración, o sea ignora el print
+    
+    print("Viendo la letra: " + letra)
+
