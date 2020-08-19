6076d48c5891ee536cd863e1f456076a1319cdc9
Lucho <quirogaluistomas@gmail.com>
2020-08-18 21:04:22 -0300

    Fin de la primera parte de POO

--- PooI.py
+++ PooI.py
@@ -1,3 +1,27 @@
-print("Mi primer commit con git")
-print("Me cago en la puta")
-print("Cambio desde Github")
+class Coche():
+    #Ponemos las propiedades o atributos
+    largoChasis = 250
+    anchoChasis = 120
+    ruedas = 4
+    enMarcha = False
+
+    #Métodos o comportamientos
+    def arrancar(self):
+        self.enMarcha = True
+
+    def estado(self):
+        if(self.enMarcha):
+            return "El coche está en marcha"
+        else:
+            return "El coche está parado"
+
+miCoche = Coche()
+
+print("El largo del coche es: " ,miCoche.largoChasis)
+print("El coche tiene ", miCoche.ruedas, " ruedas")
+miCoche.arrancar()
+
+#Verificación
+print(miCoche.estado())
+
+#Este programa tiene 4 propiedades y 2 comportamientos
