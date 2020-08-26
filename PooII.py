70b2cb871c901eb1eb93104ffe56bef170b2c04b
Lucho <quirogaluistomas@gmail.com>
2020-08-26 11:56:17 -0300

    POO segunda parte, ejemplo de auto

---
+++ PooII.py
@@ -0,0 +1,37 @@
+class Coche():
+    #Se denomina estados iniciales y se especifican mediante un constructor
+    def __init__(self): #Constructor    
+        self.__largoChasis = 250
+        self.__anchoChasis = 120
+        self.__ruedas = 4
+        self.__enMarcha = False
+        #Se ponen 2 guiones bajos para encapsular y no se puede acceder desde afuera salvo desde
+        #adentro, es decir desde un metodo o comportamiento
+
+    #Métodos o comportamientos
+    def arrancar(self, arrancamos):
+        self.enMarcha = arrancamos
+
+        if (self.__enMarcha):            
+            return "El coche está en marcha"
+        else:
+            return "El coche está parado"
+
+
+    def estado(self):
+        print("El coche tiene ", self.__ruedas, "ruedas. Un ancho de ", self.__anchoChasis, "y un largo de ", self.__largoChasis)
+
+
+miCoche = Coche()
+
+print(miCoche.arrancar(True))
+
+miCoche.estado()
+
+print("------A continuación creamos el segundo objeto------")
+
+miCoche2 = Coche()
+
+print(miCoche2.arrancar(False))
+
+miCoche2.estado()
